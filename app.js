dc066b796cd5535f87a4a775c1a75c593eddafe0
Patrycja <karppinska@gmail.com>
2018-09-05 20:54:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const 
+    submitButton = document.querySelector('.btn'),
+    input = document.querySelector('#number'),
+    dangerAlert = document.querySelector('.alert-danger'),
+    jokesContainer = document.querySelector('.jokes'),
+    validateInput = function() {
+        const isInputValid = input.value && input.value > 0;
+        if (!input.value) {
+            dangerAlert.innerText = 'Fill in input';
+        } else if (input.value < 1) {
+            dangerAlert.innerText = 'Number must be greater than zero';
+        }
+        dangerAlert.classList.toggle('hidden', isInputValid);
+        return isInputValid;
+    };
+
+input.addEventListener('change', (e) => {
+    e.preventDefault();
+    validateInput();
+});
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (validateInput()) {
+        const 
+            numberOfJokes = input.value, 
+            xhr = new XMLHttpRequest();
+
+        xhr.open('GET', `http://api.icndb.com/jokes/random/${numberOfJokes}`, true);
+        xhr.onload = function() {
+            if (this.status === 200) {
+                const jokes = JSON.parse(this.response).value;
+                jokes.forEach(element => {
+                    jokesContainer.innerHTML += `<p class="lead">${element.joke}</p>`;
+                });
+            }
+        }
+        xhr.send();
+    }
+});
